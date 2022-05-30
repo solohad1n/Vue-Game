51eb855761b091498c3337f8109067b24f0bbfdd
solohad1n <fox663399@gmail.com>
2022-05-30 20:50:49 +0600

    Added layout and game styles

---
+++ src/components/Board.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="wrapper">
+    <div class="wrapper__board">
+    <BoardItem v-for="i in 25" :key="i"/>
+  </div>
+  <div class="wrapper__control">
+    <p>Сложность: <strong>4</strong></p>
+    <button>Start</button>
+  </div>
+</div>
+</template>
+
+<script>
+import BoardItem from './BoardItem.vue';
+export default {
+    components: { BoardItem }
+}
+</script>
+
+<style>
+.wrapper__board{
+  width: 300px;
+  background: #eee;
+  margin: 0 auto;
+}
+.wrapper{
+  margin-bottom: 100px;
+}
+button{
+    background: #42b983cc;
+    color: white;
+    border-radius: 2px;
+    padding: 10px 50px;
+    margin: 10px 0;
+    cursor: pointer;
+    outline: none;
+    
+}
+button:hover{
+  background: #266e4ecc;
+  transition: background-color 0.5s, height 2s;
+}
+.wrapper__control{
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/BoardItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="item"></span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.item{
+  position: relative;
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+  background: #ccc;
+  display: inline-block;
+  cursor: pointer;
+}
+</style>
